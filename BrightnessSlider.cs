ff1d5bfc7e5055ea8b67d788422db0b5ef0b6bd9
Phil-Bastian Berndt <pehbehbeh@gmail.com>
2012-11-12 22:32:13 +0100

    BrightnessSlider und ConstrastSlider konfiguriert

---
+++ BrightnessSlider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace AP_HA
+{
+    public partial class MainWindow
+    {
+        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Console.WriteLine("Helligkeit: " + e.NewValue);
+        }
+    }
+}
---
+++ ContrastSlider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace AP_HA
+{
+    public partial class MainWindow
+    {
+        private void ContrastSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Console.WriteLine("Kontrast: " + e.NewValue);
+        }
+    }
+}
