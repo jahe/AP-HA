6e27ebefc29e3a45750d3550703443db95ed7b7a
Phil-Bastian Berndt <pehbehbeh@gmail.com>
2012-11-15 11:04:01 +0100

    SingleRandom für einen globalen Zufallswert

---
+++ util/SingleRandom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AP_HA
+{
+    class SingleRandom : Random
+    {
+        static SingleRandom _Instance;
+
+        public static SingleRandom Instance
+        {
+            get
+            {
+                if (_Instance == null) _Instance = new SingleRandom();
+                return _Instance;
+            }
+        }
+
+        private SingleRandom() {}
+    }
+}
