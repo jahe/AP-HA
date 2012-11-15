9af785cd9a3f1f7e2855fb75bde8b2ee32c4960a
Phil-Bastian Berndt <pehbehbeh@gmail.com>
2012-11-15 11:24:26 +0100

    Markierungen werden mit einer Zufallsfarbe erstellt, die in der Liste dargestellt wird

--- marks/Mark.cs
+++ marks/Mark.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Media;
 
 namespace AP_HA
 {
@@ -9,6 +10,7 @@ namespace AP_HA
     {
         string name;
         bool visible;
+        SolidColorBrush brushColor;
 
         public string Name
         {
@@ -22,9 +24,24 @@ namespace AP_HA
             set { visible = value; }
         }
 
+        public SolidColorBrush BrushColor
+        {
+            get { return brushColor; }
+            set { brushColor = value; }
+        }
+
         public Mark()
         {
             Visible = true;
+            BrushColor = getRandomColorBrush();
+        }
+
+        private SolidColorBrush getRandomColorBrush()
+        {
+            byte[] colorBytes = new byte[3];
+            SingleRandom.Instance.NextBytes(colorBytes);
+            Color randomColor = Color.FromRgb(colorBytes[0], colorBytes[1], colorBytes[2]);
+            return new SolidColorBrush(randomColor);
         }
     }
 }
