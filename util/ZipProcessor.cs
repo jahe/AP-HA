b1b339b673633e36b8bdc326c3ada73861c79ee6
Marcus Pfingsten <p83@hotmail.de>
2012-12-03 18:31:54 +0100

    commit before merge

--- util/ZipProcessor.cs
+++ util/ZipProcessor.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO.Packaging;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+
 
 namespace AP_HA
 {
@@ -14,7 +20,44 @@ namespace AP_HA
 
         public static void extractToDirectory(string sourcePath, string targetPath)
         {
-            //TO DO Unzipper implementieren
+            HausarbeitAPProjectCT deserializedXMLFile;
+            using (Package zip = Package.Open(sourcePath, FileMode.Open, FileAccess.Read)) //Zip-Datei zum lesen öffnen
+            {
+                Uri FileName = PackUriHelper.CreatePartUri(new Uri("project.xml", UriKind.Relative)); // Pfad innerhalb des zip files ist eine Url
+                PackagePart zippedFile = zip.GetPart(FileName); // Referenz auf eine Datei in dem zip file
+                deserializedXMLFile = HausarbeitAPProjectCT.CreateFromStream(zippedFile.GetStream()); // GetStream() liefert einen Stream, den wir einfach (wie z.b. einen FileStream) auslesen können.
+                for (int i = 0; i < deserializedXMLFile.totalLayers; i++) //Für die Bilddaten machen wir praktisch das gleiche mit fortlaufenden Nummern.
+                {
+                    FileName = PackUriHelper.CreatePartUri(new Uri(".\\" + i.ToString("D" + deserializedXMLFile.totalLayers.ToString("D").Length.ToString()) + ".tif", UriKind.Relative));
+                    zippedFile = zip.GetPart(FileName);
+                    Stream filestream = zippedFile.GetStream();
+
+                    string temppath = Path.Combine(targetPath, i.ToString("D" + deserializedXMLFile.totalLayers.ToString("D").Length.ToString()) + ".tif");
+                    //zippedFile.CopyTo(temppath, false);
+
+                    StreamToFile(filestream, temppath);
+                }
+            }
+            //return null;
+        }
+
+        private static void StreamToFile(Stream inputStream, string outputFile)
+        {
+            if (inputStream == null)
+                throw new ArgumentNullException("inputStream");
+
+            if (String.IsNullOrEmpty(outputFile))
+                throw new ArgumentException("Argument null or empty.", "outputFile");
+
+            using (FileStream outputStream = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+            {
+                int cnt = 0;
+                const int LEN = 4096;
+                byte[] buffer = new byte[LEN];
+
+                while ((cnt = inputStream.Read(buffer, 0, LEN)) != 0)
+                    outputStream.Write(buffer, 0, cnt);
+            }
         }
     }
 }
