92dc566381205d3ca798e857aa10add1c25b080d
Phil-Bastian Berndt <pehbehbeh@gmail.com>
2012-12-03 16:57:47 +0100

    Pen-Events werden durchgereicht

--- marks/Drawing.cs
+++ marks/Drawing.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 
 namespace AP_HA
 {
     public partial class MainWindow
     {
+        bool penButtonDown = false;
+
         private void btnPen_Click(object sender, RoutedEventArgs e)
         {
             tool = Tool.Pen;
@@ -16,6 +19,25 @@ namespace AP_HA
         private void btnEraser_Click(object sender, RoutedEventArgs e)
         {
             tool = Tool.ZoomOut;
+        }
+
+        private void penMouseLeftButtonDown(object sender, MouseEventArgs e)
+        {
+            penButtonDown = true;
+        }
+
+        private void penMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!penButtonDown)
+                return;
+
+            Point canvasPos = e.GetPosition(canvas);
+            Console.WriteLine(canvasPos);
+        }
+
+        private void penMouseLeftButtonUp(object sender, MouseEventArgs e)
+        {
+            penButtonDown = false;
         }
     }
 }
