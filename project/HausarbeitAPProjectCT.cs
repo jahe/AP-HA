511ca8efcf9d74d983d8992e2250ebd43346874a
Marcus Pfingsten <p83@hotmail.de>
2012-12-03 20:43:16 +0100

    commit to merge

--- project/HausarbeitAPProjectCT.cs
+++ project/HausarbeitAPProjectCT.cs
@@ -153,42 +153,5 @@ namespace AP_HA
                 return "Fehler";
             }
         }
-
-        /**public static object LoadFromFile(string fileName)
-        {
-            HausarbeitAPProjectCT deserializedXMLFile;
-            using (Package zip = Package.Open(fileName, FileMode.Open, FileAccess.Read)) //Zip-Datei zum lesen öffnen
-            {
-                Uri FileName = PackUriHelper.CreatePartUri(new Uri(".\\project.xml", UriKind.Relative)); // Pfad innerhalb des zip files ist eine Url
-                PackagePart zippedFile = zip.GetPart(FileName); // Referenz auf eine Datei in dem zip file
-                deserializedXMLFile = HausarbeitAPProjectCT.CreateFromStream(zippedFile.GetStream()); // GetStream() liefert einen Stream, den wir einfach (wie z.b. einen FileStream) auslesen können.
-                for (int i = 0; i < deserializedXMLFile.totalLayers; i++) //Für die Bilddaten machen wir praktisch das gleiche mit fortlaufenden Nummern.
-                {
-                   // FileName = PackUriHelper.CreatePartUri(new Uri(".\\" + i.ToZeroLeadingString(deserializedXMLFile.totalLayers.ToString().Length) + ".tif", UriKind.Relative)); // Statt einer extension Int32.ToZeroLeadingString(int länge) kann man auch irgendeine andere Funktion implementieren die die führenden nullen auffüllt
-                    //zippedFile = zip.GetPart(FileName);
-                    //Image img = Image.FromStream(zippedFile.GetStream());
-                }
-            }
-            return null;
-        }**/
-
-        /**Wenn man die Project-Klasse um Properties erweitern möchte, aber
-        //diese nicht mit in die XML-Datei schreiben will, kann man ihnen
-        //das XmlIgnoreAttribute voranstellen.
-        [XmlIgnore()]
-        public System.Drawing.Size Size
-        {
-            get
-            {
-                return new System.Drawing.Size(this.width, this.height);
-            }
-            set
-            {
-                if ((value.Width == 0) || (value.Height == 0))
-                    throw new ArgumentException();
-                this.width = value.Width;
-                this.height = value.Height;
-            }
-        }**/
     }
 }
