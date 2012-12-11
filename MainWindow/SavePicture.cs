a52b801b0b7fe8caba731e47eca1ce5e39a31736
Phil-Bastian Berndt <pehbehbeh@gmail.com>
2012-12-11 22:32:23 +0100

    Gerüst für die Methode zum Speichern der Bitmaps hinzugefügt

---
+++ MainWindow/SavePicture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AP_HA
+{
+    public partial class MainWindow
+    {
+        public void savePicture(int picNo)
+        {
+            Console.WriteLine("saving picture no: " + picNo);
+        }
+    }
+}
